a35ca0b3ab7f8ea126af2b560835491b2017b80b
ravi-dholariya <200540107103@darshan.ac.in>
2024-02-21 18:38:51 +0530

    Created Dal file and Model

---
+++ DAL/ServiceDalClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceManagement.DAL
+{
+    internal class ServiceDalClass
+    {
+        public static string connectionstring = "Data Source=LAPTOP-CMODSAPH\\SQLEXPRESS;Initial Catalog=Infinnium;Integrated Security=True;";
+
+
+    }
+}
---
+++ Model/ServiceDetailsClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceManagement.Model
+{
+    internal class ServiceDetailsClass
+    {
+        public string ServiceName {get; set;}
+        public string ServiceDescription { get; set;}
+        public string ServiceStartType { get; set;}
+        public string ServiceDisplayName { get; set;}
+    }
+}
